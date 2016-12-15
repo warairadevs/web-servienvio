3210b01f59812c3ea37089b628599e0746dc5b12
Jesus Rincon <00jjessejj00@gmail.com>
2016-12-14 22:52:11 -0400

    Fixing messages send

--- js/contact_me.js
+++ js/contact_me.js
@@ -8,6 +8,7 @@ $(function() {
         submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
+            debugger;
             var name = $("input#name").val();
             var email = $("input#email").val();
             var phone = $("input#phone").val();
